f0d22931e14f66d81f1fbf98fdad7a0cb6048ac7
Abdallah (AMEB) <ameb@odoo.com>
2025-05-05 16:55:38 +0400

    [Add] 4049583 Last purchase price
    [FIX] records IDs & LPO logic
    
    Revert "[FIX] README.md: Fixing typos"
    
    This reverts commit 4c650f360a36fd3f64edddec4caa867799704213.
    
    [CR] Fixed changes in code review

---
+++ product_last_purchase_price/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Last Purchase Price in Products and Pricelists",
+    "version": "18.0.1.0.0",
+    "summary": "Shows last purchase price on product page and adds it to pricelist options.",
+    "category": "Purchase/Purchase",
+    "author": "Odoo PS",
+    "website": "www.odoo.com",
+    "depends": ["purchase"],
+    "data": [
+        "views/product_template_views.xml",
+        "views/pricelist_item_views.xml",
+    ],
+    "license": "OEEL-1",
+}
