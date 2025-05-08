97b2119b967beebd8734f3072ecfaddc5af15a2e
Abdallah (AMEB) <ameb@odoo.com>
2025-05-08 17:59:38 +0400

    [Add] 3625842 star healthcare INVOICE reports customizations
    [CR] fixed reformated code (same thing diffirent methods)

---
+++ reports_customizations/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Reports Customizations",
+    "summary": """
+        Customize Sale order, Delivery note and Invoice reports """,
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "category": "Uncategorized",
+    "version": "18.0.1.0.0",
+    "license": "OEEL-1",
+    "depends": ["sale", "stock"],
+    "data": [
+        "report/report_invoice.xml",
+    ],
+    "task": [3625842],
+}
